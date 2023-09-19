25fc9ea78b6cf4b1582066e6cc2746848518dd39
Rajesh Chopra <rajeshchopra190@gmail.com>
2023-09-19 08:23:41 -0700

    Server url fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,4 +17,4 @@ const store = configureStore({
 
 export default store;
 
-export const server = "http://localhost:4000/api/v1";
+export const server = "https://food-corner-l6pr.onrender.com/api/v1";
